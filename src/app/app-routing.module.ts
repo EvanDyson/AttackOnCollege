90a2278ff48c558f97ea4db6b52b4bee3258c64a
EvanDyson <evandyson@ufl.edu>
2023-02-06 20:18:51 -0500

    changing files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { PageComponent } from './page/page.component';
-import { Page2Component } from './page2/page2.component'; 
 
-const routes: Routes = [
-    { path: '', component: PageComponent },
-    { path: 'page2', component: Page2Component }
-    ];
+const routes: Routes = [];
 
-    @NgModule({
-        declarations: [],
-        imports: [
-        CommonModule,
-        RouterModule.forRoot(routes)
-        ],
-        exports: [RouterModule]
-        })
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-//import { MdButtonModule, MdCardModule, MdToolbarModule } from '@angular/material';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PageComponent } from './page/page.component';
-import { Page2Component } from './page2/page2.component';
-
 @NgModule({
   declarations: [
-    AppComponent,
-    PageComponent,
-    Page2Component
+    AppComponent
   ],
-    imports: [
+  imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
