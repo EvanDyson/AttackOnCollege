671d3e3c6fa96d90464b7c06381559e2894de757
EvanDyson <evandyson@ufl.edu>
2023-02-25 18:17:48 -0500

    Revert "Adding HTTP import"
    This reverts commit bcd7e0d30494ebf17073d00cec97d27fa94b6688.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
