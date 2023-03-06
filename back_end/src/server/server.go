8ab87347043ed95c999976e179d3a2e78a9c3a95
Sara Osmanovic <73594416+SaraOsmanovic@users.noreply.github.com>
2023-03-06 20:58:04 +0000

    Updating my branch

--- back_end/src/server/server.go
+++ back_end/src/server/server.go
@@ -1,14 +1,8 @@
 package server
 
 import (
-<<<<<<< HEAD
-	"CEN3031-Project/back_end/src/controllers"
-	"CEN3031-Project/back_end/src/middlewares"
-	"os"
-=======
 	"AttackOnCollege/back_end/src/controllers"
 	"AttackOnCollege/back_end/src/middlewares"
->>>>>>> cb9a4465067009ba0ee2e0bc97ff78efc64885e7
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -16,29 +10,7 @@ import (
 
 func StartServer() {
 	r := initRouter()
-<<<<<<< HEAD
-
-	httpRouter := gin.Default()
-
-	httpRouter.GET("/:path", func(c *gin.Context) {
-		c.Redirect(302, "https://localhost:1337/"+c.Param("path"))
-	})
-	httpRouter.POST("/*path", func(c *gin.Context) {
-		c.Redirect(302, "https://localhost:1337/"+c.Param("path"))
-	})
-	httpRouter.PUT("/:path", func(c *gin.Context) {
-		c.Redirect(302, "https://localhost:1337/"+c.Param("path"))
-	})
-	httpRouter.DELETE("/:path", func(c *gin.Context) {
-		c.Redirect(302, "https://localhost:1337/"+c.Param("path"))
-	})
-
-	// Change paths once we start running the server with npm
-	go r.RunTLS(":"+os.Getenv("PORT"), "./back_end/src/server/auth/cert/cacert.crt", "./back_end/src/server/auth/cert/ca.key")
-	httpRouter.Run(":8080")
-=======
 	r.Run(":1337")
->>>>>>> ce53c2e2a020a8a5009001757e0c168bbe19f4e0
 
 }
 
