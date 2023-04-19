2b3fb684facc71c721127e090a408bcd4898d3ee
Michael Hoctor <michaelhoctor5@gmail.com>
2023-04-19 07:35:04 -0400

    Added 7 new e2e/Sprint 4 unit tests
    Added cypress tests that are both end to end and unit tests for sprint 4, a couple more to follow

--- cypress/e2e/e2e.cy.ts
+++ cypress/e2e/e2e.cy.ts
@@ -15,4 +15,89 @@ describe('template spec', () => {
         cy.get('#courseName').type("Intro to Software Engineering")
     
     })
+    it('Will get visible landing page elements',()=>{
+        cy.visit('/login')
+        cy.get("#username").type("AOCAdmin");
+        
+    
+        cy.get("#password").type("SuperSecretP4ssFor4dmin");
+        cy.get("#loginclick").click();
+        cy.location('pathname').should('eq', '/')
+        
+        cy.get('#profile').should('be.visibile');
+        cy.get('#addassignment').should('be.be.visible')
+        cy.get('#admin').should('be.visible')
+    })
+    it('Will get invisible homepage elements',()=>{
+        cy.visit('/login')
+        cy.get("#username").type("AOCAdmin");
+        
+    
+        cy.get("#password").type("SuperSecretP4ssFor4dmin");
+        cy.get("#loginclick").click();
+        cy.location('pathname').should('eq', '/')
+        cy.get('#loginwhite').should('be.hidden');
+        cy.get('#register').should('be.hidden');
+    })
+    it('Navigate to Profile Page',()=>{
+        cy.visit('/login')
+        cy.get("#username").type("AOCAdmin");
+        
+    
+        cy.get("#password").type("SuperSecretP4ssFor4dmin");
+        cy.get("#loginclick").click();
+        cy.location('pathname').should('eq', '/')
+        cy.get('#profile').click();
+        cy.location('pathname').should('eq', '/profile')
+    })
+    it('Should Log Out',()=>{
+        cy.visit('/login')
+        cy.get("#username").type("AOCAdmin");
+        
+    
+        cy.get("#password").type("SuperSecretP4ssFor4dmin");
+        cy.get("#loginclick").click();
+        cy.location('pathname').should('eq', '/')
+        cy.get('#logout').click();
+        cy.location('pathname').should('eq', '/')
+        cy.get('#profile').should('be.hidden');
+    })
+    it('Navigate to Achievement Page',()=>{
+        cy.visit('/login')
+        cy.get("#username").type("AOCAdmin");
+        
+    
+        cy.get("#password").type("SuperSecretP4ssFor4dmin");
+        cy.get("#loginclick").click();
+        cy.location('pathname').should('eq', '/')
+        cy.get('#achievementButton').click();
+        cy.location('pathname').should('eq', '/achievement')
+    })
+    it('Navigate to Edit User Page',()=>{
+        cy.visit('/login')
+        cy.get("#username").type("AOCAdmin");
+        
+    
+        cy.get("#password").type("SuperSecretP4ssFor4dmin");
+        cy.get("#loginclick").click();
+        cy.location('pathname').should('eq', '/')
+        cy.get('#profile').click();
+        cy.location('pathname').should('eq', '/profile')
+        cy.get('#edit').click();
+        cy.location('pathname').should('eq', '/edit')
+    })
+    it('Navigate to Add Assignment Page',()=>{
+        cy.visit('/login')
+        cy.get("#username").type("AOCAdmin");
+        
+    
+        cy.get("#password").type("SuperSecretP4ssFor4dmin");
+        cy.get("#loginclick").click();
+        cy.location('pathname').should('eq', '/')
+        cy.get('#profile').click();
+        cy.location('pathname').should('eq', '/profile')
+        cy.get('#addassignment').click();
+        cy.location('pathname').should('eq', '/add-assignment')
+    })
 })
+
