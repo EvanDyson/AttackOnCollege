ce1637936139851f7daecd503383f71611751365
EvanDyson <evandyson@ufl.edu>
2023-04-19 17:01:20 -0400

    added new card on profile page, card displays assignments

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+export interface assignmentStruct {
+  title: string;
+  due_date: string;
+  course_code: number;
+}
+
+const AchievementCard: assignmentStruct[] = [];
+
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent {
-  
+  columns: string[] = ['title', 'due_date', 'course_code']
+  dataSource: any;
   constructor(private http: HttpClient) {};
  
   ngOnInit() {
